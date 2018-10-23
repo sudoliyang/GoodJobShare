93589664f2ae7efc86a082df0a9b516c7951a11e
Yen-chi Chen <mark86092@gmail.com>
2018-10-23 11:18:19 +0800

    Use recompose to split route logic (ExperienceSearch)

---
+++ src/components/ExperienceSearch/withRouteParameter.js
@@ -0,0 +1,45 @@
+// @flow
+import { compose, withProps, withHandlers } from 'recompose';
+import { withRouter } from 'react-router-dom';
+import { queryParser, toQsString } from './helper';
+import { pathnameSelector, querySelector } from 'common/routing/selectors';
+
+export default compose(
+  withRouter,
+  withProps(({ location }) => {
+    const { searchBy, searchQuery, sort, page, searchType } = queryParser(
+      querySelector({ location })
+    );
+    return {
+      searchBy,
+      searchQuery,
+      sort,
+      page,
+      searchType,
+    };
+  }),
+  withHandlers({
+    changeRouteParameter: ({
+      searchBy,
+      searchQuery,
+      sort,
+      page,
+      searchType,
+      location,
+      history,
+    }) => updated => {
+      const pathname = pathnameSelector({ location });
+      const query = {
+        searchBy,
+        searchQuery,
+        sort,
+        page,
+        searchType,
+        ...updated,
+      };
+      const queryString = toQsString(query);
+      const url = `${pathname}?${queryString}`;
+      history.push(url);
+    },
+  })
+);
